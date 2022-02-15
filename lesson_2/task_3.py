72acc237aea54787df5e4c9158c72c6ecf3768a4
MashinaSSokom <skltan@mail.ru>
2022-02-15 16:07:02 +0800

    complete task_3

--- lesson_2/task_3.py
+++ lesson_2/task_3.py
@@ -11,3 +11,22 @@
 
 - Реализовать считывание данных из созданного файла и проверить, совпадают ли они с исходными.
 '''
+import yaml
+
+test_data = {'products': ['mouse', 'headphones', 'smartphone'],
+             'total_price': 3,
+             'products_price': {'mouse': '50$', 'headphones': '25$', 'smartphone': '500$'}}
+
+
+def write_to_yaml(data):
+    with open('task_3.yaml', 'w', encoding='utf-8') as f_w:
+        yaml.dump(data, f_w, default_flow_style=False, allow_unicode=True, sort_keys=False)
+
+
+def read_from_yaml(filename):
+    with open(filename, 'r', encoding='utf-8') as f_r:
+        return yaml.load(f_r, Loader=yaml.SafeLoader)
+
+
+write_to_yaml(data=test_data)
+print(test_data == read_from_yaml('task_3.yaml'))
