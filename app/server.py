912b513251d68a0ca51c7abe84609f4403d4bfae
MashinaSSokom <skltan@mail.ru>
2022-02-22 00:01:32 +0800

    fix imports

--- app/server.py
+++ app/server.py
@@ -1,16 +1,18 @@
 from socket import socket, AF_INET, SOCK_STREAM
 import time
-from app.common.utils import create_argv_parser, get_message, send_message
-from app.common.variables import DEFAULT_PORT, ACTION, TIME, USER, ACCOUNT_NAME, PRESENCE, RESPONSE, ERROR
+from common.utils import create_argv_parser, get_message, send_message
+from common.variables import DEFAULT_PORT, ACTION, TIME, USER, ACCOUNT_NAME, PRESENCE, RESPONSE, ERROR
 
 parser = create_argv_parser()
 namespace = parser.parse_args()
 
 if not namespace.p:
     namespace.p = DEFAULT_PORT
+if not namespace.a:
+    namespace.a = ''
 
 SERV_SOCK = socket(AF_INET, SOCK_STREAM)
-SERV_SOCK.bind(('', namespace.p))
+SERV_SOCK.bind((namespace.a, namespace.p))
 SERV_SOCK.listen(5)
 
 
@@ -26,12 +28,10 @@ def process_client_message(message):
     }
 
 
-process_client_message({ACTION: 'PRESENCE', USER: {ACCOUNT_NAME: 'GUEST'}, TIME: time.time()})
-
 try:
     while True:
         CLIENT_SOCK, ADDR = SERV_SOCK.accept()
-        print(f'Получен запрос на соединение от клиента с адресом и портом {ADDR}')
+        print(f'Получен запрос на коннект с {ADDR}')
         message = get_message(CLIENT_SOCK)
         response = process_client_message(message)
         send_message(CLIENT_SOCK, response)
