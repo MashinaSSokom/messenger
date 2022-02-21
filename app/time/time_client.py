392c3849f38d1935c9eaeaee3cea0edbedbeeb62
MashinaSSokom <skltan@mail.ru>
2022-02-21 23:22:00 +0800

    delete example

--- app/time/time_client.py
+++
@@ -1,7 +0,0 @@
-from socket import socket, AF_INET, SOCK_STREAM
-
-CLIENT_SOCKET = socket(AF_INET, SOCK_STREAM)
-CLIENT_SOCKET.connect(('localhost', 8888))
-TIME_BYTES = CLIENT_SOCKET.recv(1024)
-print(f'Текущее время на сервере: {TIME_BYTES.decode("utf-8")}')
-CLIENT_SOCKET.close()
