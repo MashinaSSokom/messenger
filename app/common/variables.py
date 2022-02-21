13b4dcf5e4ee27f685d1710f7d10c201a49f5a86
MashinaSSokom <skltan@mail.ru>
2022-02-21 22:40:40 +0800

    add project global variables

---
+++ app/common/variables.py
@@ -0,0 +1,20 @@
+"""Global constants"""
+
+DEFAULT_PORT = 7777
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+MAX_CONNECTIONS = 5
+MAX_PACKAGE_LENGTH = 1024
+ENCODING = 'utf-8'
+
+# JIM protocol
+
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+
+# Others
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+RESPONSE_DEFAULT_IP_ADDRESS = 'response_default_ip_address'
