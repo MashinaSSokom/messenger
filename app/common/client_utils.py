41ad12718f2be173d3f6229ff5f7a8bfeae507e6
MashinaSSokom <skltan@mail.ru>
2022-02-22 22:45:07 +0800

    add raise ValueError

--- app/common/client_utils.py
+++ app/common/client_utils.py
@@ -10,9 +10,10 @@ def create_presence(account_name='Guest'):
         USER: {ACCOUNT_NAME: account_name}
     }
 
+
 def process_response(response):
     if RESPONSE in response:
         if response[RESPONSE] == 200:
             return f'200: OK'
         return f'400: {response[ERROR]}'
-    return ValueError
+    raise ValueError
