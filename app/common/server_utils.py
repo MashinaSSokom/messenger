9493ec458f348f46b0dcd8287b8483f03e02ba1a
MashinaSSokom <skltan@mail.ru>
2022-02-27 01:03:10 +0800

    add server logging

--- app/common/server_utils.py
+++ app/common/server_utils.py
@@ -1,7 +1,15 @@
+import logging
+
 from .variables import USER, ACTION, ACCOUNT_NAME, RESPONSE, ERROR, TIME, PRESENCE
+from ..logs import config_client_log
+
+logger = logging.getLogger('server_loger')
 
 
 def process_client_message(message):
+
+    logger.debug(f'Обработка сообщения от клиента : {message}')
+
     sorted_message_keys = sorted(list(message.keys()))
     sorted_keys = sorted([USER, ACTION, TIME])
 
