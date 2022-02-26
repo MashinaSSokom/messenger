c11a50541048b4ea968ef2716af563013a24d118
MashinaSSokom <skltan@mail.ru>
2022-02-27 00:45:52 +0800

    add client logger config

--- app/logs/config_client_log.py
+++ app/logs/config_client_log.py
@@ -5,21 +5,20 @@ import logging
 
 from logging.handlers import TimedRotatingFileHandler
 
-server_log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(filename)s %(message)s')
+client_log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(filename)s %(message)s')
 
 PATH = os.path.dirname(os.path.abspath(__file__))
-PATH = os.path.join(PATH, 'server.log')
+PATH = os.path.join(PATH, 'client.log')
 
-file_handler = TimedRotatingFileHandler(PATH, encoding='utf-8', interval=1, when='D')
-file_handler.setFormatter(server_log_formatter)
+file_handler = logging.FileHandler(PATH, encoding='utf-8')
+file_handler.setFormatter(client_log_formatter)
 file_handler.setLevel(logging.DEBUG)
 
 stream_handler = logging.StreamHandler(stream=sys.stderr)
-stream_handler.setFormatter(server_log_formatter)
+stream_handler.setFormatter(client_log_formatter)
 stream_handler.setLevel(logging.ERROR)
 
-
-logger = logging.getLogger('server_logger')
+logger = logging.getLogger('client_logger')
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 logger.setLevel(logging.DEBUG)
