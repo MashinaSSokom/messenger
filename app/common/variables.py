0ea56bd105ed002389602d0c5081ab5acafad803
MashinaSSokom <skltan@mail.ru>
2022-03-26 23:43:33 +0800

    add variables for JIM

--- app/common/variables.py
+++ app/common/variables.py
@@ -4,6 +4,7 @@ DEFAULT_PORT = 7777
 DEFAULT_IP_ADDRESS = '127.0.0.1'
 MAX_CONNECTIONS = 5
 MAX_PACKAGE_LENGTH = 1024
+SERVER_TIMEOUT = 0.5
 ENCODING = 'utf-8'
 
 # JIM protocol
@@ -15,6 +16,8 @@ ACCOUNT_NAME = 'account_name'
 
 # Others
 PRESENCE = 'presence'
+MESSAGE = 'message'
+MESSAGE_TEXT = 'mess_text'
 RESPONSE = 'response'
 ERROR = 'error'
 RESPONSE_DEFAULT_IP_ADDRESS = 'response_default_ip_address'
