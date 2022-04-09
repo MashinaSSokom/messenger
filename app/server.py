e4f68bdc864fb624b2883dec69722022654261f3
MashinaSSokom <skltan@mail.ru>
2022-04-09 18:59:46 +0800

    update server core for sending destination message

--- app/server.py
+++ app/server.py
@@ -1,18 +1,23 @@
+""" SERVER LOGIC SCRIPT """
+
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 import select
 import logging
 import json
+from typing import Dict
 
-from common.server_utils import process_client_message
+from common.server_utils import process_client_message, process_sending_message
 from common.utils import create_argv_parser, get_message, send_message, create_message_to_send
-from common.variables import DEFAULT_PORT, MAX_CONNECTIONS, SERVER_TIMEOUT
+from common.variables import DEFAULT_PORT, MAX_CONNECTIONS, SERVER_TIMEOUT, DESTINATION
 from app.common.errors import IncorrectDataRecivedError
 from logs import config_server_log
 
+# Logger initialization
 logger = logging.getLogger('server_logger')
 
 
 def main():
+    # Parse launch params
     parser = create_argv_parser()
     namespace = parser.parse_args()
 
@@ -21,8 +26,9 @@ def main():
     if not namespace.a:
         namespace.a = ''
 
+    # launch server socket
     with socket(AF_INET, SOCK_STREAM) as serv_sock:
-        serv_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        serv_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)  # Reuse socket if it's busy
         serv_sock.bind((namespace.a, namespace.p))
         serv_sock.listen(MAX_CONNECTIONS)
         serv_sock.settimeout(SERVER_TIMEOUT)
@@ -33,9 +39,10 @@ def main():
 
         clients = []
         messages = []
+        client_names = {}
 
         while True:
-            # Подключение клиентов
+            # Connect clients
             try:
                 client_sock, addr = serv_sock.accept()
                 print(f'Получен запрос на коннект с {addr}')
@@ -49,12 +56,14 @@ def main():
             send_data_lst = []
             err_lst = []
 
+            # Check waiting for receive or sending clients
             try:
                 if clients:
                     recv_data_lst, send_data_lst, err_lst = select.select(clients, clients, [], 0)
             except OSError:
                 pass
 
+            # Receive client data
             if recv_data_lst:
                 for client in recv_data_lst:
                     try:
@@ -67,21 +76,30 @@ def main():
                         logger.error(f'Не удалось декодировать JSON строку, полученную от '
                                      f'клиента {client.getpeername()}. Соединение закрывается.')
                         clients.remove(client)
-                    except Exception:
+                    except Exception as e:
                         logger.error(f'Не удалось обработать сообщение: {client.getpeername()} отключился от '
-                                     f'сервера!')
+                                     f'сервера! Ошибка: {e}')
                         clients.remove(client)
 
+            # Send client data
             if messages and send_data_lst:
-                response = create_message_to_send(account_name=messages[0][0], message_text=messages[0][1])
-                del messages[0]
-                for waiting_client in send_data_lst:
+                for message in messages:
                     try:
-                        send_message(waiting_client, response)
-                    except:
-                        logger.error(f'Клиент {waiting_client.getpeername()} отключился от сервера.')
-                        waiting_client.close()
-                        clients.remove(waiting_client)
+                        process_sending_message(message, send_data_lst, client_names)
+                    except ConnectionError:
+                        logger.error(f'Связь с клиентом {message[DESTINATION]} потеряна')
+                        clients.remove(client_names[message[DESTINATION]])
+                        del client_names[message[DESTINATION]]
+                messages.clear()
+                # response = create_message_to_send(account_name=messages[0][0], message_text=messages[0][1])
+                # del messages[0]
+                # for waiting_client in send_data_lst:
+                #     try:
+                #         send_message(waiting_client, response)
+                #     except Exception:
+                #         logger.error(f'Клиент {waiting_client.getpeername()} отключился от сервера.')
+                #         waiting_client.close()
+                #         clients.remove(waiting_client)
 
 
 if __name__ == '__main__':
