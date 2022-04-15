3931f57c4a4ac3c09a9b5e055d7fd0a782eb7846
MashinaSSokom <skltan@mail.ru>
2022-04-15 23:43:16 +0800

    Translated the client part into classes

--- app/client.py
+++ app/client.py
@@ -12,63 +12,66 @@ from common.client_utils import create_presence, process_response, \
     receive_message_from_server, user_interface
 
 from common.utils import argv_parser, get_message, send_message
-from common.variables import DEFAULT_PORT, DEFAULT_IP_ADDRESS
 from common.errors import ReqFieldMissingError
 
 logger = logging.getLogger('client_logger')
 
+class Client:
 
-def main():
-    arguments = argv_parser()
+    def __init__(self, address, port, client_name):
+        self._port = port
+        self._address = address
+        self._client_name = client_name
 
-    address = arguments['address']
-    port = arguments['port']
-    client_name = arguments['client_name'] if arguments['client_name'] else input('Введите имя пользователя: ')
+    def _connect(self):
+        try:
+            client_socket = socket(AF_INET, SOCK_STREAM)
+            client_socket.connect((self._address, self._port))
 
-    logger.info(
-        f'Запущен клиент с парамертами: адрес сервера - {address}, '
-        f'порт - {port}, имя пользователя: {client_name}')
+            send_message(client_socket, create_presence(self._client_name))
+            response = get_message(client_socket)
+            logger.info(f'Клиент {self._client_name} получил от сервера сообщение {response}')
+            result = process_response(response)
+            if result == f'200: OK':
+                logger.info(f'Клиент {self._client_name}({self._address}: {self._port}) подключился к серверу с овтетом: {result}')
+                print(f'Установлено соединение с сервером')
+                print(f'Добро пожаловать, {self._client_name}! Консольный мессенджер готов к работе ^_^')
+                return client_socket
+            logger.error(f'Ошибка подключения к серверу {result}')
+            print(f'Ошибка подключения к серверу {result}. Повторите попытку позже...')
+            sys.exit(1)
+        except json.JSONDecodeError:
+            logger.error('Не удалось декодировать полученную Json строку.')
+            print('Произошла ошибка, перезапустите приложение!')
+            sys.exit(1)
+        except ReqFieldMissingError as missing_error:
+            logger.error(f'В ответе сервера отсутствует необходимое поле '
+                         f'{missing_error.missing_field}')
+            print('Произошла ошибка, перезапустите приложение!')
+            sys.exit(1)
+        except (ConnectionRefusedError, ConnectionError):
+            logger.critical(f'Не удалось подключиться к серверу {self._address}:{self._port}, '
+                            f'конечный компьютер отверг запрос на подключение.')
+            print('Не удалось подключиться к серверу!')
+            sys.exit(1)
 
-    try:
-        client_socket = socket(AF_INET, SOCK_STREAM)
-        client_socket.connect((address, port))
+    def run(self):
 
-        send_message(client_socket, create_presence(client_name))
+        logger.info(
+            f'Запукск клиента с парамертами: адрес сервера - {self._address}, '
+            f'порт - {self._port}, имя пользователя: {self._client_name}')
 
-        response = get_message(client_socket)
-        logger.info(f'Клиент {client_name} получил от сервера сообщение {response}')
-        result = process_response(response)
+        client_socket = self._connect()
 
-        logger.info(f'Клиент {client_name}({address}: {port}) подключился к серверу с овтетом: {result}')
-        print(f'Установлено соединение с сервером. Ответ сервера - {result}')
-        print(f'Добро пожаловать, {client_name}! Консольный мессенджер готов к работе ^_^')
-
-    except json.JSONDecodeError:
-        logger.error('Не удалось декодировать полученную Json строку.')
-        print('Произошла ошибка, перезапустите приложение!')
-        sys.exit(1)
-    except ReqFieldMissingError as missing_error:
-        logger.error(f'В ответе сервера отсутствует необходимое поле '
-                     f'{missing_error.missing_field}')
-        print('Произошла ошибка, перезапустите приложение!')
-        sys.exit(1)
-    except (ConnectionRefusedError, ConnectionError):
-        logger.critical(f'Не удалось подключиться к серверу {address}:{port}, '
-                        f'конечный компьютер отверг запрос на подключение.')
-        print('Не удалось подключиться к серверу!')
-        sys.exit(1)
-
-    else:
-
-        server_receiver = threading.Thread(target=receive_message_from_server, args=(client_socket, client_name))
+        server_receiver = threading.Thread(target=receive_message_from_server, args=(client_socket, self._client_name))
         server_receiver.daemon = True
         server_receiver.start()
 
-        client_interface = threading.Thread(target=user_interface, args=(client_socket, client_name))
+        client_interface = threading.Thread(target=user_interface, args=(client_socket, self._client_name))
         client_interface.daemon = True
         client_interface.start()
 
-        logger.debug(f'Клиент {client_name}({address}: {port}) завершил запуск процессов')
+        logger.debug(f'Клиент {self._client_name}({self._address}: {self._port}) завершил запуск процессов')
 
         # watchdog cycle
         while True:
@@ -76,6 +79,16 @@ def main():
             if client_interface.is_alive() and server_receiver.is_alive():
                 continue
             break
+
+
+def main():
+
+    arguments = argv_parser()
+    address = arguments['address']
+    port = arguments['port']
+    client_name = arguments['client_name'] if arguments['client_name'] else input('Введите имя пользователя: ')
+    client = Client(address, port, client_name)
+    client.run()
 
 
 if __name__ == '__main__':
