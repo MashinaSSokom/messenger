0f897a62b81f95deeefd1bdb00fc7e8fab975a65
MashinaSSokom <skltan@mail.ru>
2022-05-02 19:19:04 +0800

    add render messages stats

--- app/server.py
+++ app/server.py
@@ -20,7 +20,7 @@ from metaclasses import ServerVerifier
 from descriptors import Port
 from server_database import Storage
 
-from server_gui import MainWindow,  HistoryWindow, ConfigWindow, create_messages_stats_model, \
+from server_gui import MainWindow,  MessagesStats, ConfigWindow, create_messages_stats_model, \
     create_active_clients_model
 
 
@@ -124,40 +124,46 @@ class Server(threading.Thread, metaclass=ServerVerifier):
 
 
 def main():
-    # Parse launch params
-    arguments = argv_parser()
-    address = arguments['address']
-    port = arguments['port']
-    db = Storage()
+    try:
+        # Parse launch params
+        arguments = argv_parser()
+        address = arguments['address']
+        port = arguments['port']
+        db = Storage()
 
-    server = Server(address, port, db)
-    server.daemon = True
-    server.start()
+        server = Server(address, port, db)
+        server.daemon = True
+        server.start()
 
-    server_app = QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.statusBar().showMessage('Сервер запущен!')
-    main_window.active_clients_table.setModel(create_active_clients_model(db))
-    main_window.active_clients_table.resizeColumnsToContents()
-    main_window.active_clients_table.resizeRowsToContents()
+        server_app = QApplication(sys.argv)
+        main_window = MainWindow()
+        main_window.statusBar().showMessage('Сервер запущен!')
+        main_window.active_clients_table.setModel(create_active_clients_model(db))
+        main_window.active_clients_table.resizeColumnsToContents()
+        main_window.active_clients_table.resizeRowsToContents()
 
-    def update_active_users():
-        print('Update active users!')
-        pass
+        def update_active_users() -> None:
+            print('Update active users!')
+            pass
 
-    def show_history():
-        print('Histroy')
-        pass
+        def show_messages_stats() -> None:
+            global messages_stats
+            messages_stats = MessagesStats()
+            messages_stats.messages_stats_table.setModel(create_messages_stats_model(db))
+            messages_stats.messages_stats_table.resizeColumnsToContents()
+            messages_stats.messages_stats_table.resizeRowsToContents()
+            messages_stats.show()
 
-    def show_config():
-        print('Config')
-        pass
+        def show_config() -> None:
+            print('Config')
+            pass
 
-    main_window.refresh_button.triggered.connect(update_active_users)
-    main_window.client_history_button.triggered.connect(show_history)
-    main_window.config_button.triggered.connect(show_config)
-    server_app.exec_()
-
+        main_window.refresh_button.triggered.connect(update_active_users)
+        main_window.messages_stats_button.triggered.connect(show_messages_stats)
+        main_window.config_button.triggered.connect(show_config)
+        server_app.exec_()
+    except Exception as e:
+        print(e)
 
 
 
