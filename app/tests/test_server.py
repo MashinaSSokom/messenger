e344836d59052d827dbb23365ffc185135d0e1df
MashinaSSokom <skltan@mail.ru>
2022-05-02 20:31:05 +0800

    change method usage

--- app/tests/test_server.py
+++ app/tests/test_server.py
@@ -1,6 +1,6 @@
 import time
 import unittest
-from ..common.server_utils import process_client_message
+from server import Server
 from ..common.variables import RESPONSE, ERROR, ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME
 
 
@@ -11,23 +11,23 @@ class TestServerClass(unittest.TestCase):
         self.ok_response = {RESPONSE: 200}
 
     def test_process_right_client_message(self):
-        self.assertEqual(process_client_message({ACTION: PRESENCE, TIME: time.time(), USER: {ACCOUNT_NAME: 'Guest'}}),
+        self.assertEqual(Server._process_client_message({ACTION: PRESENCE, TIME: time.time(), USER: {ACCOUNT_NAME: 'Guest'}}),
                          self.ok_response)
 
     def test_process_wrong_action_client_message(self):
-        self.assertEqual(process_client_message({ACTION: '12aw', TIME: time.time(), USER: {ACCOUNT_NAME: 'Guest'}}),
+        self.assertEqual(Server._process_client_message({ACTION: '12aw', TIME: time.time(), USER: {ACCOUNT_NAME: 'Guest'}}),
                          self.bad_response)
 
     def test_process_no_action_client_message(self):
-        self.assertEqual(process_client_message({TIME: time.time(), USER: {ACCOUNT_NAME: 'Guest'}}),
+        self.assertEqual(Server._process_client_message({TIME: time.time(), USER: {ACCOUNT_NAME: 'Guest'}}),
                          self.bad_response)
 
     def test_process_no_time_client_message(self):
-        self.assertEqual(process_client_message({ACTION: PRESENCE, USER: {ACCOUNT_NAME: 'Guest'}}),
+        self.assertEqual(Server._process_client_message({ACTION: PRESENCE, USER: {ACCOUNT_NAME: 'Guest'}}),
                          self.bad_response)
 
     def test_process_no_user_client_message(self):
-        self.assertEqual(process_client_message({ACTION: PRESENCE, TIME: time.time()}),
+        self.assertEqual(Server._process_client_message({ACTION: PRESENCE, TIME: time.time()}),
                          self.bad_response)
 
 
